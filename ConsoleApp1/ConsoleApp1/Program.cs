87faadbb26f9750ec8bdbff81fbe6f0d84b5ee46
berktu <53702213+berktu-akc@users.noreply.github.com>
2021-12-16 14:23:29 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort sp = new SerialPort();
+            sp.PortName = "COM4";
+            sp.BaudRate = 9600;
+            sp.DataBits = 8;
+            sp.Parity = Parity.None;
+            sp.StopBits = StopBits.One;
+
+            sp.Open();
+
+            while (true)
+            {
+                var readData = sp.ReadLine();
+                Console.WriteLine(readData);
+            }
+        }
+    }
+}
